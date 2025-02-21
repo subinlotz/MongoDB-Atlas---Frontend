9128c21631cce965bc38646e5675d72e42436cc7
subinlotz <lotzsubin@gmail.com>
2025-02-21 13:42:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Github Backend Repo https://github.com/subinlotz/MongoDB-Atlas---Backend
+
+Deployed Frontend Url https://authjwt1.netlify.app/
+
+Deployed Backend Url https://mongodb-atlas-backend.onrender.com
---
+++ src/components/Signup.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./Signup.css";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("https://mongodb-atlas-backend.onrender.com/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    
+    if (data.status === "ok") {
+      alert('Your data saved to our database')
+    }
+    if (data.status ==="error") {
+      alert("user already exists")
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          name="name"
+          placeholder="Full Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
